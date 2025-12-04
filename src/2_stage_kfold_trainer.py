f7221c2e6e9a59487081846a43a164871f90ed7c
wajih <wajihullahbaig@gmail.com>
2025-12-04 14:40:56 +0400

    missing logger added

--- src/2_stage_kfold_trainer.py
+++ src/2_stage_kfold_trainer.py
@@ -407,7 +407,7 @@ class Stage2Dataset(Dataset):
          # 2. Sample Weights 
         if self.use_weights:
             # Note: We calculate weights based on pred_species balance
-            self.df, _ = calculate_sample_weights(self.df, group_col='pred_species',smooth=5.0, logger=None)
+            self.df, _ = calculate_sample_weights(self.df, group_col='pred_species',smooth=5.0, logger=logger)
         else:
             self.df['sample_weight'] = 1.0
         

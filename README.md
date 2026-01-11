b57d5b2643cccda9ce808f94958c9b18432ace76
wajih <wajihullahbaig@gmail.com>
2026-01-11 09:06:46 +0400

    kaggle: 0.46

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ https://www.kaggle.com/competitions/csiro-biomass/discussion?sort=votes
 
 ## 1
 
-Kaggle: routed predictor 0.39
-
+Kaggle: State_Sampling_Date, f4, f1 fold only, routed predictor 0.44
+Kaggle: State_Sampling_Date, f4, f 1,2,3,4, routed predictor 0.46
--- inference/local_inference_router.py
+++ inference/local_inference_router.py
@@ -27,7 +27,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # PATHS (Update MODEL_DIR to your upload location)
 TEST_CSV_PATH = './test.csv'  
 TEST_IMG_DIR = './test/' 
-MODEL_DIR = './logs/unified_holdout_20260110_105450'
+MODEL_DIR = './logs/unified_holdout_20260110_224819'
 
 # DEFAULTS
 IMAGE_HEIGHT = 256

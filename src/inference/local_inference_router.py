40c4dd35e556c9b71b57ffc5b19e0793ac5888a8
wajih <wajihullahbaig@gmail.com>
2026-01-19 17:39:51 +0400

    kaggle:  0.55

--- src/inference/local_inference_router.py
+++ src/inference/local_inference_router.py
@@ -27,7 +27,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # PATHS (Update MODEL_DIR to your upload location)
 TEST_CSV_PATH = './test.csv'  
 TEST_IMG_DIR = './test/' 
-MODEL_DIR = './logs/unified_holdout_20260119_143530'
+MODEL_DIR = './logs/unified_holdout_20260119_144853'
 
 # DEFAULTS
 IMAGE_HEIGHT = 256
